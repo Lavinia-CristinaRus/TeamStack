d291fa741c2562629ddf76b9ec17f05d4ce8906e
dianachismore <diana.chismore@gmail.com>
2022-04-11 23:45:22 +0300

    structure

---
+++ teamstack/src/pages/Login.js
@@ -0,0 +1,37 @@
+import logo from 'C:/Users/diana/OneDrive/Desktop/TeamStack/teamstack/src/logo.svg';
+
+function Login() {
+  return (
+    <div className="Login">
+      <div>
+    <h1>Log In</h1>
+    <form>
+        <div>
+            <div>
+              <label for="email">Email Adress</label>
+            </div>
+            <div>
+              <input type="email" id="email" name="email" placeholder="Your email adress.."></input>
+            </div>
+          </div>
+          <div>
+            <div>
+              <label for="pass">Password</label>
+            </div>
+            <div class="col2">
+              <input type="password" id="pass" name="pass" placeholder="Your password.."></input>
+            </div>
+          </div>
+            <input type="submit" value="Log In"></input>
+        </form>
+</div>
+<a href="javascript:setTimeout(()=>{window.location = '/register' },200);">
+    <div>
+        <h1 id = "register_text">Register</h1>
+    </div>
+</a>
+    </div>
+  );
+}
+
+export default Login;
