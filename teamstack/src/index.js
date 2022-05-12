afd2c87511f9d2f0837648b23c649efbc78d532d
Lavinia-CristinaRus <lavinia.rus20@gmail.com>
2022-05-12 23:59:30 +0300

    connection to database and function to maintain it

--- teamstack/src/index.js
+++ teamstack/src/index.js
@@ -3,6 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const session = require('express-session');
+const port = process.env.PORT || 3000;
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'db'
+});
+
+function handleDisconnect() {
+
+  connection.connect(function(err) {              
+    if(err) {                                     
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    } 
+    else {
+      console.log("Database connected successfully");
+    }                                   
+  });                                     
+
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                     
+    } else {                                  
+      throw err;                                 
+    }
+  });
+}
+
+setInterval(function () {
+connection.query('SELECT 1');
+}, 5000);
+
+handleDisconnect();
+
+
 
 ReactDOM.render(
   <React.StrictMode>
